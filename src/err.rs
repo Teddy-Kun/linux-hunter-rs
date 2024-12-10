10d6170e114694d414ee7edb620ea5c8018769e4
Paul Orzel <pb.orzel@proton.me>
2024-12-10 21:11:17 +0100

    add backtrace as optional feature

--- src/err.rs
+++ src/err.rs
@@ -1,25 +1,41 @@
 use std::fmt::Display;
 
+#[cfg(feature = "backtrace")]
 use backtrace::Backtrace;
 
 #[derive(Debug)]
 pub struct Error {
 	message: String,
+	#[cfg(feature = "backtrace")]
 	trace: Backtrace,
 }
 
 impl Error {
+	#[cfg(feature = "backtrace")]
 	pub fn new(message: &str) -> Self {
 		Self {
 			message: message.to_string(),
 			trace: Backtrace::new(),
 		}
 	}
+
+	#[cfg(not(feature = "backtrace"))]
+	pub fn new(message: &str) -> Self {
+		Self {
+			message: message.to_string(),
+		}
+	}
 }
 
 impl Display for Error {
+	#[cfg(feature = "backtrace")]
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(f, "{}:\n{:#?}", self.message, self.trace)
+	}
+
+	#[cfg(not(feature = "backtrace"))]
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}:", self.message)
 	}
 }
 
