caf085bd1bff8d1df8d5b673df1c78cd459a54c4
Paul Orzel <pb.orzel@proton.me>
2024-12-15 14:31:50 +0100

    fix other patterns

--- src/memory/pattern.rs
+++ src/memory/pattern.rs
@@ -95,56 +95,75 @@ pub fn find_player_name(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Err
 pub fn find_current_player_name(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x0D];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		take::<_, _, nom::error::Error<&[u8]>>(initial_bytes.len()),
 		take(4usize),
 		tag(&[0x48, 0x8D, 0x55]),
 		take(1usize),
 		tag(&[0x45, 0x31, 0xC9, 0x41, 0x89, 0xC0, 0xE8]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([res.0, res.1, res.2, res.3, res.4].concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => return Ok([res.0, res.1, res.2, res.3, res.4].concat()),
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 // 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B D8 48 85 C0 75 04 33 C9
 pub fn find_player_damage(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x0D];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		tag::<_, _, nom::error::Error<&[u8]>>(initial_bytes),
 		take(4usize),
 		tag(&[0xE8]),
 		take(4usize),
 		tag(&[0x48, 0x8B, 0xD8, 0x48, 0x85, 0xC0, 0x75, 0x04, 0x33, 0xC9]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([res.0, res.1, res.2, res.3, res.4].concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => return Ok([res.0, res.1, res.2, res.3, res.4].concat()),
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 // 48 8B 0D ?? ?? ?? ?? B2 01 E8 ?? ?? ?? ?? C6 83 ?? ?? ?? ?? ?? 48 8B 0D
 pub fn find_monster(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x0D];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		tag::<_, _, nom::error::Error<&[u8]>>(initial_bytes),
 		take(4usize),
 		tag(&[0xB2, 0x01, 0xE8]),
@@ -152,45 +171,65 @@ pub fn find_monster(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>>
 		tag(&[0xC6, 0x83]),
 		take(5usize),
 		tag(&[0x48, 0x8B, 0x0D]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([res.0, res.1, res.2, res.3, res.4, res.5, res.6].concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => return Ok([res.0, res.1, res.2, res.3, res.4, res.5, res.6].concat()),
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 // 48 8B 05 ?? ?? ?? ?? 41 8B 94 00 ?? ?? ?? ?? 89 57
 pub fn find_player_buff(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x05];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		tag::<_, _, nom::error::Error<&[u8]>>(initial_bytes),
 		take(4usize),
 		tag(&[0x41, 0x8B, 0x94, 0x00]),
 		take(4usize),
 		tag(&[0x89, 0x57]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([res.0, res.1, res.2, res.3, res.4].concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => return Ok([res.0, res.1, res.2, res.3, res.4].concat()),
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 // 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4E ?? F3 0F 10 86 ?? ?? ?? ?? F3 0F 58 86 ?? ?? ?? ?? F3 0F 11 86 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4E
 pub fn find_lobby_status(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x0D];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		tag::<_, _, nom::error::Error<&[u8]>>(initial_bytes),
 		take(4usize),
 		tag(&[0xE8]),
@@ -206,19 +245,33 @@ pub fn find_lobby_status(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Er
 		tag(&[0xE8]),
 		take(4usize),
 		tag(&[0x48, 0x8B, 0x4E]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([
-			res.0, res.1, res.2, res.3, res.4, res.5, res.6, res.7, res.8, res.9, res.10, res.11,
-			res.12, res.13, res.14,
-		]
-		.concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => {
+				return Ok([
+					res.0, res.1, res.2, res.3, res.4, res.5, res.6, res.7, res.8, res.9, res.10,
+					res.11, res.12, res.13, res.14,
+				]
+				.concat())
+			}
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 // 45 6D 65 74 74 61
 pub fn find_emetta(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+	// TODO: potentially modify this to be faster, after fixing it in the first place
 	let initial_bytes = [0x45, 0x6D, 0x65, 0x74, 0x74, 0x61];
 
 	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
@@ -236,12 +289,11 @@ pub fn find_emetta(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> 
 pub fn find_player_name_linux(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 	let initial_bytes = [0x48, 0x8B, 0x0D];
 
-	let sliced = match take_until::<_, _, nom::error::Error<&[u8]>>(&initial_bytes[..])(input) {
-		Ok((res, _)) => res,
-		Err(_) => return Err(Error::new("pattern not found").into()),
-	};
+	let mut sliced;
+	let pos = get_search_index(&initial_bytes, input)?;
+	sliced = &input[pos..];
 
-	match tuple((
+	let mut pattern = tuple((
 		tag::<_, _, nom::error::Error<&[u8]>>(initial_bytes),
 		take(4usize),
 		tag(&[0x48, 0x8D, 0x54, 0x24]),
@@ -249,11 +301,22 @@ pub fn find_player_name_linux(input: &[u8]) -> Result<Vec<u8>, Box<dyn std::erro
 		tag(&[
 			0x48, 0x8B, 0x5C, 0x24, 0x60, 0x48, 0x83, 0xC4, 0x50, 0x5F, 0xC3,
 		]),
-	))(sliced)
-	{
-		Ok((_, res)) => Ok([res.0, res.1, res.2, res.3, res.4].concat()),
-		Err(_) => Err(Error::new("pattern not found").into()),
+	));
+
+	// since nom fails to find the pattern, if it matches the first ones partially, but the rest does not match, we need to try again, after removing the first bytes
+	while sliced.len() > 37 {
+		match pattern(sliced) {
+			Ok((_, res)) => return Ok([res.0, res.1, res.2, res.3, res.4].concat()),
+			Err(_) => {
+				// we can safely skip the same amount of bytes that we search for, since we failed to find it
+				sliced = &sliced[37..];
+				let pos = get_search_index(&initial_bytes, sliced)?;
+				sliced = &sliced[pos..];
+			}
+		}
 	}
+
+	Err(Error::new("pattern not found").into())
 }
 
 #[cfg(test)]
